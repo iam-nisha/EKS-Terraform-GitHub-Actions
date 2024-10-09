64f60406054e3e7e7351cf30ca0fcf504f595604
iam-nisha <namdeonishajbp@gmail.com>
2024-10-09 17:35:30 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,6 +10,7 @@ terraform {
     bucket         = "mynisbucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
     encrypt        = true
   }
 }
